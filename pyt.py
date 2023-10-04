ef4627f16ea305003eb0b089c42697029462b668
n0mega <102227669+n0mega@users.noreply.github.com>
2023-10-04 12:15:54 +0300

    Update pyt.py

--- pyt.py
+++ pyt.py
@@ -1,3 +1,12 @@
 def reverse(x):
     return x[::-1]
-print(reverse("Fonk"))
+
+
+def cheack_plaindorme(s):
+    result = []
+    for i in len(s):
+        for j in range(i + 1, len(s) + 1):
+            sub = s[i:j]
+            if reverse(sub):
+                result.append(sub)
+    return result
